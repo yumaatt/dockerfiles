5995a8c45aae202c91fbb761688520ed85c61899
yumaatt <yumaatt@gmail.com>
2016-02-12 07:30:12 +0900

    Add setup.sh

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # dockerfiles
+
+# how to use
+
+cd /path/to/dockerfiles/test
+docker-compose build
+docker-compose up -d
+./setup.sh
---
+++ setup.sh
@@ -0,0 +1,13 @@
+TEST_HOST1=192.168.2.10
+TEST_HOST2=192.168.2.11
+
+# pipework
+sudo pipework br1 ubuntu14 "${TEST_HOST1}/24"
+sudo pipework br1 centos6 "${TEST_HOST2}/24"
+sudo ip addr add 192.168.2.1/24 dev br1
+
+# stone
+sudo stone -p "${TEST_HOST1}:80" 10080 &
+sudo stone -p "${TEST_HOST1}:443" 10443 &
+sudo stone -p "${TEST_HOST2}:80" 11080 &
+sudo stone -p "${TEST_HOST2}:443" 11443 &
